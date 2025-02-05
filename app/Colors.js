4918f2b9cf885a5860dc0bcfcd31144f79f9bbf8
Satyen0592 <satyen212@gmail.com>
2025-02-05 23:22:06 +0530

    added addList Box code

--- app/Colors.js
+++ app/Colors.js
@@ -1,6 +1,6 @@
 export default Colors = {
-  black: "#2D3436",
-  blue: "24A6D9",
-  lightblue: "#A7CBD9",
-  white: "#FFFFFF",
+  black: "rgba(45, 52, 54, 1)",
+  blue: "rgba(36, 166, 217, 1)",
+  lightBlue: "rgba(167, 203, 217, 1)",
+  white: "rgba(255, 255, 255, 1)",
 };
