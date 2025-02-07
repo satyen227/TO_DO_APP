89520d105355eb4280f2057170d87aaa515d3b41
Satyen0592 <satyen212@gmail.com>
2025-02-07 20:30:20 +0530

    adding content in List

--- app/components/TodoList.js
+++ app/components/TodoList.js
@@ -1,12 +1,25 @@
-import { react } from "react";
+import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import Colors from "../Colors";
 
 export default TodoList = ({ list }) => {
+  const completedCount = list.todos.filter((todo) => todo.completed).length;
+  const remainingCount = list.todos.length - completedCount;
   return (
     <View style={[styles.listContainer, { backgroundColor: list.color }]}>
       <Text style={styles.listTitle} numberOfLines={1}>
         {list.name}
       </Text>
+      <View>
+        <View style={{ alignItems: "center" }}>
+          <Text style={styles.count}>{remainingCount}</Text>
+          <Text style={styles.subtitle}>Remaining</Text>
+        </View>
+        <View style={{ alignItems: "center" }}>
+          <Text style={styles.count}>{completedCount}</Text>
+          <Text style={styles.subtitle}>Completed</Text>
+        </View>
+      </View>
     </View>
   );
 };
@@ -25,5 +38,15 @@ const styles = StyleSheet.create({
     fontWeight: "700",
     color: Colors.white,
     marginBottom: 18,
+  },
+  count: {
+    fontSize: 48,
+    fontWeight: 200,
+    color: Colors.white,
+  },
+  subtitle: {
+    fontSize: 12,
+    fontWeight: 700,
+    color: Colors.white,
   },
 });
